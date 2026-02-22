85064646e161b62a500e29baf128468d46fe6014
moc <lev.ustinov@gmail.com>
2026-02-21 22:30:00 -0500

    a function

--- ts/PaymentStatus.ts
+++ ts/PaymentStatus.ts
@@ -1,3 +1,4 @@
+
 type PaymentStatus = "PAID" | "PENDING" | "FAILED";
 
 function isSuccessful(status: PaymentStatus): boolean {
@@ -7,3 +8,12 @@ function isSuccessful(status: PaymentStatus): boolean {
 type Payment =
   | { type: "card"; cardNumber: string; amount: number }
   | { type: "paypal"; email: string; amount: number };
+
+function processPayment(payment:Payment): PaymentStatus{
+
+    if(payment.type === "card" && (payment.cardNumber.length < 4 || payment.amount > 5000)) return "FAILED";
+    if (payment.type === "paypal" && !payment.email.includes("@")) return "FAILED";
+    return "PAID";
+};
+
+
